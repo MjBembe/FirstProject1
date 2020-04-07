0d7f66b6193118a425b4a700e4e2988e14954e09
MJ Bembe <mncedisie@gmail.com>
2020-04-07 14:03:12 +0200

    first commit of an ADD app

--- ADD.java
+++ ADD.java
@@ -2,10 +2,10 @@ public class ADD{
 	public static void main(String[] args){
 
 		int x =3;
-		int y=7;
+		int y= 10;
 		int  z = y+ x;
 
-		System.out.println("x=3 + y=7 is equal to "+z);
+		System.out.println("x=3 + y=10 is equal to "+z);
 	}
 
 
